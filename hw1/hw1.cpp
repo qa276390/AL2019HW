c628deea1857a7e1160317682ff1b896c1c5f0d1
qa276390 <b04505026@ntu.edu.tw>
2019-04-20 21:50:55 +0800

    segfault on dorm

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -4,22 +4,25 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
+#include "node.h"
 using namespace std;
 
-void showGraph(char *graph,int w,int h);
-
+void showGraph(Node **graph,int w,int h);
+void outputGraph(Node **graph,int w,int h, int ncc, string outfile);
+int BFScoloring(Node **graph, int x, int y, char tcolor,char ncolor,int w, int h, int* count);
+int BFS(Node **graph, int x, int y, char tcolor,int w, int h);
 int main (int argc, char* argv[]) {
   	string line;
   	int w = 0;
   	int h = 0;
-  	char *graph;
+  	Node **graph;
   	stringstream ss;
 
 
 	string infile = argv[1];
 	int x = atoi(argv[2]);
 	int y = atoi(argv[3]);
-	char color = argv[4][0];
+	char ncolor = argv[4][0];
 	string outfile = argv[5];
   	
 	ifstream myfile(infile.c_str());
@@ -36,7 +39,9 @@ int main (int argc, char* argv[]) {
 				ss.clear();
 				ss<<line;
 				ss>>w>>h;
-				graph = new char [w*h];	
+				graph = new Node* [w*h];
+				for(int i=0;i<w*h;i++)
+				{graph[i] = new Node();}
 			}
 			else
 			{
@@ -45,17 +50,42 @@ int main (int argc, char* argv[]) {
 				ss<<line;
 				while(ss>>tmp)
 				{	//ss>>tmp;
-					graph[idx] = tmp;
+					graph[idx]->setcolor(tmp);
 					idx++;	
 				}
 			}
 
-    		cout << line << '\n';
+    		//cout << line << '\n';
 		//cout<<"w="<<w<<", h="<<h<<endl;
     	}
-		cout<<"w="<<w<<", h="<<h<<endl;
-		showGraph(graph, w, h);
-
+		//cout<<"w="<<w<<", h="<<h<<endl;
+		//showGraph(graph, w, h);
+		
+		int count = 0;
+		char tcolor = graph[y*w+x]->getcolor();
+		BFScoloring(graph, x, y, tcolor, ncolor, w, h, &count);
+		int ncc = 0;
+		//cout<<endl;
+		//showGraph(graph, w, h);
+		for(int i=0;i<w*h;i++)
+		{graph[i]->setflag(false);}
+		for(int i=0;i<h;i++)
+		{
+			for(int j=0;j<w;j++){
+				if(graph[i*w+j]->getflag()==false)
+				{
+					ncc++;
+					BFS(graph, j, i, graph[i*w+j]->getcolor(), w, h);
+					//cout<<j<<"_"<<i<<endl;
+				}
+			}
+		
+		}
+		//cout<<ncc<<endl;
+		outputGraph(graph, w, h, ncc, outfile);
+		for(int i=0;i<w*h;i++)
+		{delete graph[i];}
+		delete[] graph;
     	myfile.close();
   	}else 
 		cout << "Unable to open file"; 
@@ -63,12 +93,95 @@ int main (int argc, char* argv[]) {
   return 0;
 }
 
-void showGraph(char *graph,int w,int h)
+int BFS(Node **graph, int x, int y, char tcolor,int w, int h)
+{	
+	int ind = y*w+x;
+	//cout<<"("<<x<<","<<y<<") "<<ind<<"<"<<w*h<<endl;
+	
+	if(x<0||y<0||x>=w||y>=h){
+			return 2;//out of bound
+	}
+	if(graph==NULL)
+	{
+		cout<<"1.NULL!"<<endl;
+		exit(0);
+	}
+	if(graph[ind]==NULL)
+	{
+		cout<<"NULL!"<<endl;
+		exit(0);
+	}
+	if(graph[ind]->getflag()==true)
+			return 1;//already found
+	if(graph[ind]->getcolor()==tcolor){
+		graph[ind]->setflag(true);
+	//up
+		BFS(graph, x, y-1, tcolor, w, h);
+	//down
+		BFS(graph, x, y+1, tcolor, w, h);
+	//left
+		BFS(graph, x-1, y, tcolor, w, h);
+	//right
+		BFS(graph, x+1, y, tcolor, w, h);
+	}
+	else
+	{	
+		return 0;//not the color we are looking for
+	}
+
+}
+int BFScoloring(Node **graph, int x, int y, char tcolor,char ncolor,int w, int h, int* c)
+{
+	int ind = y*w+x;
+	//cout<<"("<<x<<","<<y<<") "<<*c<<endl;
+	if(x<0||y<0||x>=w||y>=h)
+			return 2;//out of bound
+	if(graph[ind]->getflag()==true)
+			return 1;//already found
+	if(graph[ind]->getcolor()==tcolor){
+		graph[ind]->setcolor(ncolor);
+		graph[ind]->setflag(true);
+		(*c)++;
+	//up
+		BFScoloring(graph, x, y-1, tcolor, ncolor, w, h, c);
+	//down
+		BFScoloring(graph, x, y+1, tcolor, ncolor, w, h, c);
+	//left
+		BFScoloring(graph, x-1, y, tcolor, ncolor, w, h, c);
+	//right
+		BFScoloring(graph, x+1, y, tcolor, ncolor, w, h, c);
+	}
+	else
+	{	
+		return 0;//not the color we are looking for
+	}
+
+}
+
+void showGraph(Node **graph,int w,int h)
 {
 	for(int i=0;i<w*h;i++)
 	{
-		cout<<char(graph[i])<<" ";
+		graph[i]->getflag();
+		cout<<char(graph[i]->getcolor())<<" ";
 		if((i+1)%w==0)
 			cout<<endl;
 	}
+}
+
+void outputGraph(Node **graph,int w,int h, int ncc, string outfile)
+{
+	ofstream ofile(outfile.c_str());
+	if(ofile.is_open())
+	{
+		ofile << w << ' ' << h <<"\n";
+		for(int i=0;i<w*h;i++)
+		{
+			ofile<<char(graph[i]->getcolor())<<" ";
+			if((i+1)%w==0)
+				ofile<<"\n";
+		}
+		ofile<<ncc;
+		ofile.close();
+	}else cout<<"Unable to open file"<<endl;
 }
--- hw1/test.sh
+++ hw1/test.sh
@@ -1,1 +1,10 @@
-./paintBucket sample.in 1 3 B sample1.out
+input=$1
+x=$2
+y=$3
+c=$4
+echo "$input"
+echo "start!"
+./paintBucket $input $x $y $c sample.out
+./paintBucket-ref $input $x $y $c sample-ref.out
+diff sample.out sample-ref.out
+echo "finish!"
